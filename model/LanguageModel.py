2cf2fa401ad37c2fafdc974762d05099be9e2110
Andrew Cranston <andrew.cranston@digio.com.au>
2023-03-25 19:55:38 +1100

    Changes to the main language model class

--- model/LanguageModel.py
+++ model/LanguageModel.py
@@ -4,6 +4,74 @@ from torch import nn
 from model.AutoTuneMemory import AutoMemoryModule
 
 
+class RNNEncoderBlock(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(RNNEncoderBlock, self).__init__()
+        self.rnn = nn.GRU(input_size, hidden_size, batch_first=True)
+        self.bn = nn.BatchNorm1d(hidden_size)
+        self.relu = nn.ReLU()
+        
+    def forward(self, x):
+        out, _ = self.rnn(x)
+        out = self.bn(out[:, -1, :])
+        out = self.relu(out)
+        return out
+
+class MemoryEncoder(nn.Module):
+    def __init__(self, input_size, hidden_size, memory_size):
+        super(MemoryEncoder, self).__init__()
+        self.rnn_blocks = nn.Sequential(
+            RNNEncoderBlock(input_size, hidden_size),
+            RNNEncoderBlock(hidden_size, hidden_size),
+            RNNEncoderBlock(hidden_size, hidden_size),
+            RNNEncoderBlock(hidden_size, hidden_size)
+        )
+        self.linear = nn.Linear(hidden_size, memory_size)
+        self.concat_bn = nn.BatchNorm1d(input_size + memory_size)
+        self.fc_blocks = nn.Sequential(
+            nn.Linear(input_size + memory_size, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            nn.Linear(256, 128),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Linear(128, 64),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.Linear(64, 32),
+            nn.BatchNorm1d(32),
+            nn.ReLU(),
+            nn.Linear(32, input_size)
+        )
+        
+    def forward(self, x, memory_context):
+        # Pass input_tokens through RNN encoder blocks
+        out = self.rnn_blocks(x)
+        out = self.linear(out)
+        # Combine memory_context and RNN output
+        out = torch.cat([out, memory_context], dim=1)
+        out = self.concat_bn(out)
+        # Pass through fully connected layers
+        out = self.fc_blocks(out)
+        return out
+
+class Decoder(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(Decoder, self).__init__()
+        self.hidden_size = hidden_size
+        self.embedding = nn.Embedding(output_size, input_size)
+        self.gru = nn.GRU(input_size, hidden_size, batch_first=True)
+        self.linear = nn.Linear(hidden_size, output_size)
+        
+    def forward(self, x, hidden):
+        # Embed input token
+        x = self.embedding(x)
+        # Pass through GRU layer
+        out, hidden = self.gru(x, hidden)
+        # Pass through linear layer and return output
+        out = self.linear(out[:, -1, :])
+        return out, hidden
+
 class LanguageModel(nn.Module):
     def __init__(self, vocab_size, token_dim, hidden_dim, num_layers, max_memory_size, padding_token=0):
         super(LanguageModel, self).__init__()
@@ -16,45 +84,18 @@ class LanguageModel(nn.Module):
         # Initialize the AutoMemoryModule
         self.memory_module = AutoMemoryModule(token_dim, hidden_dim, max_memory_size, padding_token)
 
-        # Initialize the encoder
-        encoder_layers = []
-        for i in range(num_layers):
-            encoder_layers.append(nn.GRU(input_size=max_memory_size + token_dim, hidden_size=hidden_dim, batch_first=True))
-            encoder_layers.append(nn.BatchNorm1d(hidden_dim))
-            encoder_layers.append(nn.ReLU())
-            encoder_layers.append(nn.Linear(hidden_dim, token_dim))
-            encoder_layers.append(nn.BatchNorm1d(token_dim))
-            encoder_layers.append(nn.ReLU())
-        self.encoder = nn.Sequential(*encoder_layers)
+        self.memory_encoder = MemoryEncoder(token_dim, hidden_dim, max_memory_size)
 
-        # Initialize the decoder
-        decoder_layers = []
-        for i in range(num_layers):
-            decoder_layers.append(nn.Linear(token_dim, hidden_dim))
-            decoder_layers.append(nn.BatchNorm1d(hidden_dim))
-            decoder_layers.append(nn.ReLU())
-            decoder_layers.append(nn.Linear(hidden_dim, token_dim))
-            decoder_layers.append(nn.BatchNorm1d(token_dim))
-            decoder_layers.append(nn.ReLU())
-        self.decoder = nn.Sequential(*decoder_layers)
-
-        # Initialize the output layer
-        self.output_layer = nn.Linear(token_dim, vocab_size)
+        self.decoder = Decoder(token_dim, hidden_dim, vocab_size)
 
     def forward(self, input_tokens, memory_context):
         # Pass the input tokens and memory context through the AutoMemoryModule
         memory_context, _ = self.memory_module(input_tokens, memory_context)
 
-        # Concatenate the input tokens with the memory context
-        combined_input = torch.cat((input_tokens, memory_context), dim=0)
-
-        # Pass the concatenated tokens through the encoder
-        encoder_output, _ = self.encoder(combined_input.unsqueeze(0).unsqueeze(-1).permute(0, 2, 1).reshape(1, -1, self.token_dim + self.max_memory_size))
+        # Pass the input tokens and memory context through the MemoryEncoder
+        encoded_context = self.memory_encoder(input_tokens, memory_context)
 
         # Pass the encoder output through the decoder
-        decoder_output = self.decoder(encoder_output)
+        decoder_output = self.decoder(encoded_context)
 
-        # Compute the logit representing the next predicted token
-        output_logit = self.output_layer(decoder_output.squeeze(0))
-
-        return output_logit, memory_context
+        return decoder_output, memory_context
